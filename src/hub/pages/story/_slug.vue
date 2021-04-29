f8a4ac6b9dd8428819e1b0cf5f1dec651e089ee9
Mathina <43380411+blaznstein@users.noreply.github.com>
2021-04-29 22:40:47 +0200

    Enable scroll if story does not fit into window

--- src/hub/pages/story/_slug.vue
+++ src/hub/pages/story/_slug.vue
@@ -299,16 +299,6 @@ export default {
 .root {
   height: 100%;
   width: 100%;
-
-  &::before {
-    content: '';
-    width: 200vw;
-    height: 200vh;
-    position: fixed;
-    z-index: -1;
-    left: -100vw;
-    top: -100vh;
-  }
 }
 
 .text-wrapper {
