e0e578f6ad1c656c852229d1dc89836a06365f24
Daniel Ramos <daniel.ramosg@gmail.com>
2021-06-02 16:50:48 +0200

    Unified translations in one file

---
+++ src/hub/static/apps/imaginary/translation.js
@@ -0,0 +1,38 @@
+translation = {
+    "de": {
+        "Btn_Back": "Zurück!",
+        "Btn_Go": "Los!",
+        "Btn_Add": "Merken!",
+        "Btn_Delete": "Löschen!",
+        "flamma": ["", "", "Sehr gut!", "", "", "Toll!", "", "", "Wow!", "", "", "Weiter so!", "", "", "Großartig!", ""],
+        "flamma2": ["Super!", "", "Sehr gut!", "", "", "Toll!", "", "", "Wow!", "", "", "Weiter so!", "", "", "Großartig!", ""],
+        "wrongChoice": "Schade! Es wäre die hier gewesen.",
+    },
+    "en": {
+        "Btn_Back": "Back!",
+        "Btn_Go": "Go!",
+        "Btn_Add": "Add!",
+        "Btn_Delete": "Delete!",
+        "flamma": ["", "", "Good!", "", "", "Great!", "", "", "Wow!", "", "", "Super!", "", "", "Amazing!", ""],
+        "flamma2": ["Great!", "", "Good!", "", "", "Great!", "", "", "Wow!", "", "", "Super!", "", "", "Amazing!", ""],
+        "wrongChoice": "Oh no! It would have been this one.",
+    },
+    "it": {
+        "Btn_Back": "Back!",
+        "Btn_Go": "Go!",
+        "Btn_Add": "Add!",
+        "Btn_Delete": "Delete!",
+        "flamma": ["", "", "Good!", "", "", "Great!", "", "", "Wow!", "", "", "Super!", "", "", "Amazing!", ""],
+        "flamma2": ["Great!", "", "Good!", "", "", "Great!", "", "", "Wow!", "", "", "Super!", "", "", "Amazing!", ""],
+        "wrongChoice": "Oh no! It would have been this one.",
+    },
+    "pt": {
+        "Btn_Back": "Back!",
+        "Btn_Go": "Go!",
+        "Btn_Add": "Add!",
+        "Btn_Delete": "Delete!",
+        "flamma": ["", "", "Good!", "", "", "Great!", "", "", "Wow!", "", "", "Super!", "", "", "Amazing!", ""],
+        "flamma2": ["Great!", "", "Good!", "", "", "Great!", "", "", "Wow!", "", "", "Super!", "", "", "Amazing!", ""],
+        "wrongChoice": "Oh no! It would have been this one.",
+    }
+};
