725a38ab0b111ff53623d803f702935da02eb5bc
Riccardo <rmoschetti@gmail.com>
2021-06-03 09:30:17 +0200

    Update translation.js translations in italian of gemran apps

--- src/hub/static/apps/imaginary/translation.js
+++ src/hub/static/apps/imaginary/translation.js
@@ -16,12 +16,12 @@ translation = {
         "wrongChoice": "Oh no! It would have been this one.",
     },
     "it": {
-      "Btn_Back": "Back!",
-      "Btn_Go": "Go!",
-      "Btn_Add": "Add!",
-      "Btn_Delete": "Delete!",
-      "flamma": ["Well done!", "", "Good!", "", "", "Great!", "", "", "Wow!", "", "", "Super!", "", "", "Amazing!", ""],
-      "wrongChoice": "Oh no! It would have been this one.",
+      "Btn_Back": "Indietro!",
+      "Btn_Go": "Vai!",
+      "Btn_Add": "Aggiungi!",
+      "Btn_Delete": "Elimina!",
+      "flamma": ["Ben fatto!", "", "Bene!", "", "", "Ottimo!", "", "", "Wow!", "", "", "Super!", "", "", "Fantastico!", ""],
+      "wrongChoice": "Oh no! Doveva essere questa.",
     },
     "pt": {
       "Btn_Back": "Back!",
