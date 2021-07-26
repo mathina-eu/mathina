6c6810481a1ae79eb97ea5e17301ed65caa7d6c8
blaznstein <cyber.bp@gmail.com>
2021-07-27 00:56:03 +0200

    Add landing page
    Migration of landing page with addition of new features and translatable content

---
+++ src/landing/i18n/en.js
@@ -0,0 +1,50 @@
+export default {
+  landing: {
+    headline: 'An interactive storybook between mathematics and fantasy',
+    videoLink: 'https://www.youtube.com/embed/zBKHA9GSfNE',
+    bigButton: 'Discover the Adventures of Mathina',
+    bigButtonLink: 'https://mathina-hub.netlify.app/',
+    eduButton: 'Educator’s guide',
+    eduButtonLink: 'https://mathina-edu.netlify.app/',
+    shareButton: 'Share your ideas and contact us on Facebook',
+    shareButtonLink: 'https://www.facebook.com/Mathinaland',
+    firstSection: '<h1 class="title">Mathina promotes mathematical thinking by developing, implementing, and ' +
+      'disseminating open educational tools adequate to the digital age.</h1><p>Mathina is based on story-driven ' +
+      'experiences, in which children and young learners encounter fictional characters that find themselves in ' +
+      'mathematical adventures. The stories bring up concepts, challenges, and puzzles that the user can interact with, ' +
+      'reflect on and learn from.</p><p>Mathina can be used by parents to play with their kids, by teachers as a ' +
+      'didactical tool, or directly by young learners eager to discover more maths, reinforcing the connection between ' +
+      'formal and non-formal learning of mathematics.</p>',
+    secondSection: '<p>The Mathina project offers the following materials for young learners, parents, and educators:</p>' +
+      '<ul>' +
+      '<li>The Interactive Storybook <strong><a href="https://mathina-hub.netlify.app/" target="_blank">The Adventures ' +
+      'of Mathina</a></strong>. With stories to read and interactive apps to play and learn. Stories have a recommended' +
+      ' minimum age: from 4 years old, from 7 years, from 11 years, and from 15 years.</li>' +
+      '<li>The <strong><a href="https://mathina-edu.netlify.app/" target="_blank">Educator’s online guide</a></strong>.' +
+      ' A companion for the interactive storybook that parents and teachers can use to extract the most out of the ' +
+      'mathematical ideas presented in the children’s book.</li>' +
+      '<li>The booklet <strong><a href="https://mathina.eu/documents/Booklet_Mathina.pdf?v=1584032400" target="_blank">' +
+      'Mathina’s World</a></strong> (pdf), a presentation of Mathina’s pedagogical principles, for parents and ' +
+      'educators. Why fiction stories? Why these specific math topics? Why would you offer Mathina to your children?</li>' +
+      '<li>The <strong>Handbook for educators</strong> (pdf, soon online), with additional material, explanations,' +
+      ' and resources.</li>' +
+      '</ul>' +
+      '<p>Find out also our <a href="https://www.youtube.com/channel/UCHQLGzygvMILa7VJARAbEMg" target="_blank">' +
+      'YouTube channel</a> and our <a href="https://www.facebook.com/Mathinaland" target="_blank">Facebook page</a>.</p>',
+    logoSectionText: 'Mathina is realized in the framework of an ' +
+      '<a href="https://ec.europa.eu/programmes/erasmus-plus/projects/eplus-project-details/#project/2018-1-DE03-KA201-047397" ' +
+      'target="_blank">Erasmus+ project</a> between 2018-2021, funded by the European Commission. Five partners ' +
+      'collaborate to realize Mathina:',
+    footerSection: '<h1 class="title">The Open Educational Resources developed within Mathina are released under free ' +
+      'and open licenses (Creative Commons BY-NC-SA, Apache 2.0)</h1>' +
+      '<p>Contact: You can post your experiences and queries about using the materials on our ' +
+      '<a href="https://www.facebook.com/Mathinaland" target="_blank">Facebook page</a>. You can also contact us ' +
+      'privately at <a href="mailto:mathinaproject@gmail.com">MathinaProject@gmail.com</a>.</p>',
+    footerLinks: '<p><a href="https://mathina-hub.netlify.app/contributors" target="_blank">Contributors</a></p>' +
+      '<p><a href="">Impressum</a></p>' +
+      '<p><a href="">Privacy policy</a></p>',
+  },
+  disclaimer: 'This project has been funded with support from the European Commission. This publication reflects the ' +
+    'views only of the author, and the Commission cannot be held responsible for any use which may be made of the ' +
+    'information contained therein.',
+};
---
+++ src/landing/pages/index.vue
@@ -0,0 +1,151 @@
+<template>
+  <div>
+    <section class="header">
+      <logo width="250px" height="auto"/>
+      <v-list-item-group
+        :value="activeLocale"
+        class="language-selection"
+      >
+        <v-list-item
+          v-for="locale in $i18n.locales"
+          :key="locale.code"
+          :value="locale.code"
+          :to="switchLocalePath(locale.code)"
+          link
+        >
+          <v-list-item-content>
+            {{ locale.code }}
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </section>
+    <div class="slider">
+      <div class="container">
+        <section class="slider__item">
+          <h1 class="slider__title">
+            {{ $t('landing.headline') }}
+          </h1>
+          <div class="slider__image">
+            <slider-image />
+          </div>
+        </section>
+      </div>
+    </div>
+    <div class="container">
+      <div class="content">
+        <section class="buttons-section">
+          <a
+            class="button button--big"
+            :href="$t('landing.bigButtonLink')"
+            target="_blank"
+          >{{ $t('landing.bigButton') }}</a>
+        </section>
+<!--        <section class="buttons-section buttons-section&#45;&#45;secondary">-->
+<!--          <a-->
+<!--            class="button button&#45;&#45;secondary"-->
+<!--            :href="$t('landing.eduButtonLink')"-->
+<!--            target="_blank"-->
+<!--          >Educator’s guide</a>-->
+<!--          <a-->
+<!--            class="button button&#45;&#45;secondary"-->
+<!--            :href="$t('landing.shareButtonLink')"-->
+<!--            target="_blank"-->
+<!--          >{{ $t('landing.shareButton') }}</a>-->
+<!--        </section>-->
+        <div class="separator" />
+        <section v-html="$t('landing.firstSection')" />
+        <section class="video-section">
+          <div class="video-container">
+            <iframe
+              :src="$t('landing.videoLink')"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          </div>
+        </section>
+        <section v-html="$t('landing.secondSection')" />
+        <div class="separator" />
+        <section class="partners">
+          <p
+            class="text--small"
+            v-html="$t('landing.logoSectionText')"
+          >
+            Mathina is realized in the framework of an Erasmus+ project between 2019-2021. <br>
+            Five partners collaborate to realize Mathina:
+          </p>
+          <a href="https://www.atractor.pt/">
+            <img class="partners__image"
+                 width="133"
+                 height="157"
+                 src="~assets/images/atractor.png"
+            >
+          </a>
+          <span class="partners__name">Atractor (Portugal)</span>
+          <a href="https://bragi.si/">
+            <img class="partners__image"
+                 width="93"
+                 height="62"
+                 src="~assets/images/bragi.png"
+            >
+          </a>
+          <span class="partners__name">Bragi Vizualne Komunikacije (Slovenia)</span>
+          <a href="https://www.curvilinea.org/">
+            <img class="partners__image"
+                 width="178"
+                 height="75"
+                 src="~assets/images/curvilinea.png"
+            >
+          </a>
+          <span class="partners__name">Curvilinea Società Cooperativa (Italy)</span>
+          <a href="http://www.elmenymuhely.hu/">
+            <img class="partners__image"
+                 width="111"
+                 height="111"
+                 src="~assets/images/experience_workshop.png"
+            >
+          </a>
+          <span class="partners__name">Experience Workshop (Finland)</span>
+          <a href="https://imaginary.org/">
+            <img class="partners__image"
+                 width="138"
+                 height="29"
+                 src="~assets/images/imaginary.png"
+            >
+          </a>
+          <span class="partners__name">IMAGINARY (Germany)</span>
+        </section>
+        <div class="separator"></div>
+        <section  v-html="$t('landing.footerSection')" />
+        <section
+          class="footer-section"
+          v-html="$t('landing.footerLinks')"
+        />
+      </div>
+    </div>
+    <section class="footer">
+      <div class="footer__text">
+        {{ $t('disclaimer') }}
+      </div>
+      <div class="footer__image">
+        <img width="193"
+             height="44"
+             src="~assets/images/erasmus_programme.png"
+        >
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue';
+import SliderImage from '../components/sliderImage';
+
+export default {
+  components: {
+    SliderImage,
+    Logo,
+  }
+};
+</script>
